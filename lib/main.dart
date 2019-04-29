0932a2bb50d3f72018715b4c7ab16218bbb4f0af
EYoung75 <eyoung75.ey@gmail.com>
2019-04-29 16:45:44 -0600

    setup basic flutter environment. Created file structure to contain screens. Rendered basic UI for Landing page

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+import "./screens/landing.dart";
+import "./screens/home.dart";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        accentColor: Colors.blueGrey[300],
+      ),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        "/": (BuildContext context) => Landing(),
+        "/home": (BuildContext context) => Home()
+      }
+    );
+  }
+}
---
+++ lib/screens/landing.dart
@@ -0,0 +1,44 @@
+import "package:flutter/material.dart";
+
+class Landing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("PinPoint")),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage("https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_645,q_50,w_1024/v1/clients/denver/4f616c91_3217_4da7_807e_ede1e41bf98e_276dbd3a-8822-49ba-9246-41767b077386.jpg"),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Center(
+            child: Card(
+              elevation: 50,
+              margin: EdgeInsets.fromLTRB(50, 150, 50, 200),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                      leading: Icon(
+                    Icons.location_on,
+                  ),
+                  title: Text("PinPoint"),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(40, 20, 40, 45),
+                    child: Text(
+                      "Click the button below, type in a keyword, and find matching locations closest to you"
+                    ),
+                  ),
+                  RaisedButton(
+                    elevation: 10,
+                    child: Text("Start Searching"),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
