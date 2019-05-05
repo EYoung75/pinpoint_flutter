44294c24722284ee071213fea20482f1f456c8a9
EYoung75 <eyoung75.ey@gmail.com>
2019-05-05 12:51:16 -0600

    increased radius parameter in api fetch request and seemed to (at least marginally) fix known flutter index range error

--- lib/screens/place.dart
+++ lib/screens/place.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:latlong/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 class Place extends StatelessWidget {
   final place;
@@ -12,65 +14,90 @@ class Place extends StatelessWidget {
         title: Text(place["name"]),
       ),
       body: Center(
-        child: Container(
-          constraints: BoxConstraints.expand(),
-          margin: EdgeInsets.fromLTRB(10, 30, 10, 0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 500,
-                width: 500,
-                padding: EdgeInsets.all(10),
-                child: Card(
-                  elevation: 15,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        place["name"],
-                        style: TextStyle(fontSize: 35, color: Colors.black,),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        place["categories"][0]["name"],
-                        style: TextStyle(fontSize: 25),
-                      ),
-                      SizedBox(
-                        height: 35,
-                      ),
-                      GestureDetector(
-                        onTap: () {launch("https://maps.google.com/?q=${place["location"]["address"]}");},
-                                              child: place["location"]["address"] != null
-                            ? Text(place["location"]["address"],
-                                style: TextStyle(fontSize: 20))
-                            : Text(
-                                "The address for this location has not been listed"),
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
-                    ],
+          child: Container(
+            constraints: BoxConstraints.expand(),
+            margin: EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child: Column(
+      children: <Widget>[
+        Container(
+          height: 200,
+          width: 500,
+          padding: EdgeInsets.all(10),
+          child: Card(
+            elevation: 15,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  place["name"],
+                  style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.black,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              RaisedButton(
-                elevation: 10,
-                child: Text("Navigate"),
-                onPressed: () {
-                  String query = place["location"]["address"];
-                  String url = "https://maps.google.com/?q=${query}";
-                  print(query);
-                  launch(url);
-                },
-              )
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  place["categories"][0]["name"],
+                  style: TextStyle(fontSize: 25),
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    launch(
+                        "https://maps.google.com/?q=${place["location"]["address"]}");
+                  },
+                  child: place["location"]["address"] != null
+                      ? Text(place["location"]["address"],
+                          style: TextStyle(fontSize: 20))
+                      : Text(
+                          "The address for this location has not been listed"),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
           ),
         ),
-      ),
+        RaisedButton(
+          elevation: 10,
+          child: Text("Navigate"),
+          onPressed: () {
+            String query = place["location"]["address"];
+            String url = "https://maps.google.com/?q=${query}";
+            print(query);
+            launch(url);
+          },
+        ),
+        Flexible(
+              child: FlutterMap(
+                options: MapOptions(
+                  center: LatLng(56.704173, 11.543808),
+                  minZoom: 12.0,
+                  maxZoom: 14.0,
+                  zoom: 0.0,
+                  swPanBoundary: LatLng(56.6877, 11.5089),
+                  nePanBoundary: LatLng(56.7378, 11.6644),
+                ),
+                layers: [
+                  TileLayerOptions(
+                    offlineMode: true,
+                    maxZoom: 14.0,
+                    urlTemplate: 'assets/map/anholt_osmbright/{z}/{x}/{y}.png',
+                  ),
+                ],
+              ),
+            ),
+      ],
+            ),
+          ),
+        ),
     );
   }
 }
--- lib/screens/results.dart
+++ lib/screens/results.dart
@@ -5,7 +5,6 @@ import "package:http/http.dart" as http;
 import "../util/utils.dart" as util;
 import "./place.dart";
 
-
 class Results extends StatefulWidget {
   final String searchTerm;
   final currentLocation;
@@ -23,16 +22,18 @@ class _ResultsState extends State<Results> {
         title: Text("Results for ${widget.searchTerm} near you"),
       ),
       body: Center(
-        child: Container(child: updateResults(widget.searchTerm, widget.currentLocation)),
+        child: Container(
+            child: updateResults(widget.searchTerm, widget.currentLocation)),
       ),
     );
   }
 
-  Future<Map> getResults(String clientId, String clientSecret, String searchTerm, var currentLocation) async {
+  Future<Map> getResults(String clientId, String clientSecret,
+      String searchTerm, var currentLocation) async {
     print("${currentLocation.latitude},${currentLocation.longitude}");
     print("ACCURACY: ${currentLocation.accuracy}");
     String apiUrl =
-        "https://api.foursquare.com/v2/venues/search?client_id=${util.clientId}&client_secret=${util.clientSecret}&v=20180323&ll=${currentLocation.latitude},${currentLocation.longitude}&intent=browse&radius=250&llAcc=1000.0&query=${searchTerm}";
+        "https://api.foursquare.com/v2/venues/search?client_id=${util.clientId}&client_secret=${util.clientSecret}&v=20180323&ll=${currentLocation.latitude},${currentLocation.longitude}&intent=browse&radius=1000&llAcc=1000.0&query=${searchTerm}";
     http.Response response = await http.get(apiUrl);
     // print(json.decode(response.body));
     return json.decode(response.body);
@@ -40,45 +41,62 @@ class _ResultsState extends State<Results> {
 
   Widget updateResults(String searchTerm, var currentLocation) {
     return FutureBuilder(
-      future: getResults(util.clientId, util.clientSecret, searchTerm, currentLocation),
+      future: getResults(
+          util.clientId, util.clientSecret, searchTerm, currentLocation),
       builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
         if (snapshot.hasData) {
           var content = snapshot.data["response"]["venues"];
-          print(content.length);
-          return Container(
-            child: ListView.builder(
-              itemCount: 7,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Place(content[index])));
-                  },
-                  child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 40),
-                      child: Center(
-                        child: Card(
-                          child: Column(
-                            children: <Widget>[
-                              ListTile(
-                                leading: Icon(Icons.store),
-                                title: Text(content[index]["name"]),
+          if (content.length <= 0) {
+            return Center(
+                child: Container(
+              child: Text(
+                "No results were returned for that keyword. Please try another.",
+                textAlign: TextAlign.center,
+              ),
+            ));
+          } else {
+            return Column(
+              children: <Widget>[
+                Flexible(
+                  child: ListView.builder(
+                    itemCount: content.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      if(content[index] == 0 || index > content.length ) { return Text("");}
+                      else {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Place(content[index])));
+                        },
+                        child: Container(
+                            margin: EdgeInsets.symmetric(horizontal: 40),
+                            child: Center(
+                              child: Card(
+                                child: Column(
+                                  children: <Widget>[
+                                    ListTile(
+                                      leading: Icon(Icons.store),
+                                      title: Text(content[index]["name"]),
+                                    ),
+                                    Text(
+                                        "Tags: ${content[index]["categories"][0]["name"]}"),
+                                    SizedBox(
+                                      height: 15,
+                                    )
+                                  ],
+                                ),
                               ),
-                              Text(
-                                  "Tags: ${content[index]["categories"][0]["name"]}"),
-                              SizedBox(
-                                height: 15,
-                              )
-                            ],
-                          ),
-                        ),
-                      )),
-                );
-              },
-            ),
-          );
+                            )),
+                      );
+                      }
+                    },
+                  ),
+                ),
+              ],
+            );
+          }
         } else {
           return Center(
             child: CircularProgressIndicator(),
