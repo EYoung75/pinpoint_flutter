cebdb55a2b248753bf18fbab5657e12d3cb20736
EYoung75 <eyoung75.ey@gmail.com>
2019-05-07 15:57:27 -0600

    configured map settings and controls

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -4,7 +4,6 @@ import "package:flutter/material.dart";
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Map extends StatefulWidget {
-  @override
   final place;
   Map(this.place);
 
@@ -14,42 +13,49 @@ class Map extends StatefulWidget {
 class _MapState extends State<Map> {
   Completer<GoogleMapController> _controller = Completer();
   LatLng _center;
+  LatLng _offset;
   Set<Marker> _markers = {};
 
   void initState() {
     super.initState();
     setState(() {
-      _center = LatLng(widget.place["location"]["lat"], widget.place["location"]["lng"]);
+      _center = LatLng(
+          widget.place["location"]["lat"], widget.place["location"]["lng"]);
+      _offset = LatLng(widget.place["location"]["lat"] + .0009,
+          widget.place["location"]["lng"]);
       _markers.add(Marker(
-        // This marker id can be anything that uniquely identifies each marker.
-        markerId: MarkerId(_center.toString()),
-        position: _center,
-        infoWindow: InfoWindow(
-          title: widget.place["name"],
-          snippet: '4 Star Rating',
-        ),
-        icon: BitmapDescriptor.defaultMarker,
-      ));
-      _center = LatLng(widget.place["location"]["lat"]+.0009, widget.place["location"]["lng"]);
+          markerId: MarkerId(_center.toString()),
+          position: _center,
+          infoWindow: InfoWindow(
+            title: widget.place["location"]["address"],
+            snippet: "4 star rating",
+          ),
+          onTap: () {
+            CameraPosition(target: _offset);
+          },
+          consumeTapEvents: false,
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed)));
     });
   }
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
-    print("LOCATION: ${widget.place["location"]["lat"]}");
-    
   }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      mapType: MapType.normal,
+      tiltGesturesEnabled: false,
       markers: _markers,
       myLocationButtonEnabled: false,
       rotateGesturesEnabled: false,
       scrollGesturesEnabled: false,
+      zoomGesturesEnabled: false,
       onMapCreated: _onMapCreated,
       initialCameraPosition: CameraPosition(
-        target: _center,
+        target: _offset,
         zoom: 18.0,
       ),
     );
