38f8c2f532fe347b529442b53e5b332013712f3f
EYoung75 <eyoung75.ey@gmail.com>
2019-05-07 15:03:44 -0600

    successfully rendered marker on place location

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -13,25 +13,39 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
   Completer<GoogleMapController> _controller = Completer();
-
   LatLng _center = LatLng(37.7825, -122.4077);
+  Set<Marker> _markers = {};
 
   void initState() {
     super.initState();
+    setState(() {
+      _center = LatLng(widget.place["location"]["lat"], widget.place["location"]["lng"]);
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_center.toString()),
+        position: _center,
+        infoWindow: InfoWindow(
+          title: widget.place["name"],
+          snippet: '5 Star Rating',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
   }
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
     print("LOCATION: ${widget.place["location"]["lat"]}");
+    
   }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
-      
+      markers: _markers,
       onMapCreated: _onMapCreated,
       initialCameraPosition: CameraPosition(
-        target: LatLng(widget.place["location"]["lat"], widget.place["location"]["lng"]),
+        target: _center,
         zoom: 18.0,
       ),
     );
