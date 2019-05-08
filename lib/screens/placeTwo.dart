d461f43b957afa46fc5e9f5cb86e6838a65e7356
EYoung75 <eyoung75.ey@gmail.com>
2019-05-08 13:39:16 -0600

    successfully reproduced place card using information populated via goole place details api

--- lib/screens/placeTwo.dart
+++ lib/screens/placeTwo.dart
@@ -43,8 +43,6 @@ class _PlaceState extends State<Place> {
     http.Response response = await http.get(apiUrl);
     // print(json.decode(response.body));
     return json.decode(response.body);
-    // var cool = json.decode(response.body);
-    // print("COOL ${cool["results"]}");
   }
 
   Widget updateResults(place) {
@@ -54,7 +52,86 @@ class _PlaceState extends State<Place> {
         if(snapshot.hasData) {
           var content = snapshot.data["result"];
           print("DATA$content");
-          return Container();
+          return Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 350,
+                width: 600,
+                margin: EdgeInsets.fromLTRB(20, 45, 20, 0),
+                child: Card(
+                  elevation: 25,
+                  child: Stack(
+                    children: <Widget>[
+                      // Container(padding: EdgeInsets.all(5), child: Icon(Icons.attach_money)),
+                      renderCost(place["price_level"]),
+                      Container(
+                        decoration: BoxDecoration(border: Border.all(color: Colors.black), borderRadius: BorderRadius.circular(3)),
+                        padding: EdgeInsets.all(10),
+                        constraints: BoxConstraints.expand(),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Text(
+                              place["name"],
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+
+                            // Text(
+                            //   place["categories"][0]["name"],
+                            //   style: TextStyle(fontSize: 25),
+                            // ),
+
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text("${place["types"][0].toUpperCase()}",
+                                    style: TextStyle(fontSize: 25)),
+                              ],
+                            ),
+                            renderRatings(place["rating"].round()),
+                            Text("${place["rating"].toString()}"),
+                            Text(
+                                "(${place["user_ratings_total"].toString()}  ratings)"),
+                            Text(
+                              place["formatted_address"],
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: 30,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              RaisedButton(
+                color: Colors.redAccent[200],
+                textColor: Colors.white,
+                elevation: 25,
+                child: Text(
+                  "Navigate",
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  
+                },
+              ),
+            ],
+          ),
+        );
         } else {
           return Center(
             child: CircularProgressIndicator(),
@@ -62,5 +139,43 @@ class _PlaceState extends State<Place> {
         }
       },
     );
+  }
+
+
+  Widget renderRatings(var ratings) {
+    List<Widget> stars = List<Widget>();
+    if (ratings != null) {
+      for (var i = .5; i <= ratings; i++) {
+        stars.add(Icon(
+          Icons.star,
+          color: Colors.amber,
+        ));
+      }
+      return Row(
+        children: stars,
+        mainAxisAlignment: MainAxisAlignment.center,
+      );
+    } else {
+      return Text("No Ratings for this location");
+    }
+  }
+
+  Widget renderCost(var price) {
+    if (price != null) {
+      List<Widget> cost = List<Widget>();
+      for (var i = 1; i <= price; i++) {
+        cost.add(Icon(
+          Icons.attach_money,
+          color: Colors.black,
+        ));
+      }
+      return Container(
+        padding: EdgeInsets.fromLTRB(5, 5, 0, 0),
+        child: Row(
+          children: cost,
+        ),
+      );
+    } else
+      return Text("");
   }
 }
