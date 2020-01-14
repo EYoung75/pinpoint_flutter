eb1a525098255872722a9b3336af135fd95188b0
EYoung75 <eyoung75.ey@gmail.com>
2020-01-13 19:36:48 -0600

    tweaked home scaffold to avoid resizing of background image

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -27,6 +27,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(
           "Enter a keyword",
